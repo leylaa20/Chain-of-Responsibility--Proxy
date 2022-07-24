dc97a01a6911a7da1bb1786b7b065aef187f327e
Leyla <leylaqocayeva28@.com>
2022-07-24 23:19:34 +0400

    Add project files.

---
+++ Chain of Responsibility/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace Chain_of_Responsibility;
+
+class PurchaseOrder
+{
+    public int RequestNumber { get; set; }
+    public double Amount { get; set; }
+    public double Price { get; set; }
+    public string Name { get; set; }
+
+    public PurchaseOrder(int number, double amount, double price, string name)
+    {
+        RequestNumber = number;
+        Amount = amount;
+        Price = price;
+        Name = name;
+
+        Console.WriteLine("\nPurchase request for " + name + " (" + amount + " for $"
+                          + price.ToString() + ") has been submitted");
+    }
+}
+
+abstract class Approver
+{
+    protected Approver? Supervisor;
+
+    public void SetSupervisor(Approver supervisor)
+    {
+        Supervisor = supervisor;
+    }
+
+    public abstract void ProcessRequest(PurchaseOrder purchase);
+}
+
+class HeadChef : Approver
+{
+    public override void ProcessRequest(PurchaseOrder purchase)
+    {
+        if (purchase.Price < 1000)
+            Console.WriteLine($"{GetType().Name} approved purchase request #{purchase.RequestNumber}");
+
+        else if (Supervisor != null)
+            Supervisor.ProcessRequest(purchase);
+    }
+}
+
+class PurchasingManager : Approver
+{
+    public override void ProcessRequest(PurchaseOrder purchase)
+    {
+        if (purchase.Price < 2000)
+            Console.WriteLine($"{GetType().Name} approved purchase request #{purchase.RequestNumber}");
+
+        else if (Supervisor != null)
+            Supervisor.ProcessRequest(purchase);
+
+    }
+}
+
+class GeneralManager : Approver
+{
+    public override void ProcessRequest(PurchaseOrder purchase)
+    {
+        if (purchase.Price < 10000)
+            Console.WriteLine($"{GetType().Name} approved purchase request #{purchase.RequestNumber}");
+
+        else
+            Console.WriteLine($"Purchase request #{purchase.RequestNumber} requires an meeting");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Approver leyla = new HeadChef();
+        Approver aris = new PurchasingManager();
+        Approver asel = new GeneralManager();
+
+        // responsibility chain burda yaranir
+        leyla.SetSupervisor(aris);
+        aris.SetSupervisor(asel);
+
+        PurchaseOrder p = new PurchaseOrder(1, 20, 69, "fruit");
+        leyla.ProcessRequest(p);
+
+        p = new PurchaseOrder(2, 300, 1389, "vegetable");
+        leyla.ProcessRequest(p);
+
+        p = new PurchaseOrder(3, 500, 5624, "beef");
+        leyla.ProcessRequest(p);
+
+        p = new PurchaseOrder(4, 4, 15198, "ovens");
+        leyla.ProcessRequest(p);
+
+        Console.ReadKey();
+    }
+}
---
+++ Proxy/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace Proxy;
+
+public interface ISharedFolder
+{
+    void PerformRWOperations();
+}
+
+public class SharedFolder : ISharedFolder
+{
+    public void PerformRWOperations()
+    {
+        Console.WriteLine("Performing Read Write operation on the Shared Folder");
+    }
+}
+
+public class Employee
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Role { get; set; }
+
+    public Employee(string username, string password, string role)
+    {
+        Username = username;
+        Password = password;
+        Role = role;
+    }
+}
+
+class SharedFolderProxy : ISharedFolder
+{
+    private ISharedFolder _folder;
+    private Employee _employee;
+    public SharedFolderProxy(Employee emp)
+    {
+        _employee = emp;
+    }
+
+    public void PerformRWOperations()
+    {
+        if (_employee.Role.ToUpper() == "CEO" || _employee.Role.ToUpper() == "MANAGER")
+        {
+            _folder = new SharedFolder();
+            Console.WriteLine("PerformRWOperations method");
+            _folder.PerformRWOperations();
+        }
+
+        else Console.WriteLine("You don't have permission to access this folder");
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Client passing employee with Role Developer to folderproxy...");
+
+        Employee emp1 = new("Aris", "Aris123", "Developer");
+        SharedFolderProxy folderProxy1 = new(emp1);
+
+        folderProxy1.PerformRWOperations();
+
+        Console.WriteLine("\nClient passing employee with Role Manager to folderproxy...");
+
+        Employee emp2 = new("Asel", "Asel123", "Manager");
+        SharedFolderProxy folderProxy2 = new(emp2);
+
+        folderProxy2.PerformRWOperations();
+    }
+}
